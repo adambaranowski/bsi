e3aef0b764889cb8ca85c52235930f4e4ea92732
andrzejwl <andrzejw239@gmail.com>
2022-11-27 20:12:19 +0100

    fetch session data

--- backend/src/main/resources/static/js/index.js
+++ backend/src/main/resources/static/js/index.js
@@ -1,9 +1,28 @@
-// Get the modal
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
+    }
+}
+
+async function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    const response = await fetch(`/login?username=${username}&password=${password}`);
+
+    const data = await response.json();
+
+    console.log(data.sessionId);
+    sessionStorage.setItem("sessionId", data.sessionId);
+    setSessionId();
+    document.getElementById('id01').style.display='none'
+}
+
+function setSessionId() {
+    let token = sessionStorage.getItem("sessionId");
+
+    if (token !== null) {
+        document.getElementById("session-data").innerText = "Session token is: "+token;
     }
 }
